e2de262176a5a40a43bbd60427aa108c7e4f8d4b
Maria Theresa Carreon <122862523+theacarreon@users.noreply.github.com>
2024-10-06 08:39:44 +0000

    Laravel Framework Login and Register

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
+$url = config('app.url');
+URL::forceRootUrl($url);
 
 Route::get('/', function () {
     return view('welcome');
